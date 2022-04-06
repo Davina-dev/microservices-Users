96c48dc76f63dcd7b53a06112688f5f612b7aae0
Davina-dev <davinamr91@gmail.com>
2022-04-06 18:26:46 +0200

    folder structure

---
+++ src/test/java/es/edu/code_sherpas/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.edu.code_sherpas.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
