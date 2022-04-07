8037707ada51c9b241f1bfcd91fc152aa1669d0b
Davina-dev <davinamr91@gmail.com>
2022-04-07 16:49:36 +0200

    Revert "Revert "add hateoas (create link to navigation)""
    This reverts commit ebf782876ac5ec0346982d3acde9f0fb58e049f0.

--- src/main/java/es/edu/code_sherpas/microservices/modelo/UserDTO.java
+++ src/main/java/es/edu/code_sherpas/microservices/modelo/UserDTO.java
@@ -1,11 +1,12 @@
 package es.edu.code_sherpas.microservices.modelo;
 
 import lombok.*;
+import org.springframework.hateoas.RepresentationModel; // para que sea navegable: extends RepresentationModel
 
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
-public class UserDTO {
+public class UserDTO extends RepresentationModel<UserDTO> {
 
     @NonNull
     private int id;
